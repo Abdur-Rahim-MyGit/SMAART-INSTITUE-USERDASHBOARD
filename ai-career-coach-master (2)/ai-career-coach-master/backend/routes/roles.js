dc09a0c9e8c55ec7b9a882acc8c85488a6d21df0
Abdur-Rahim-MyGit <rahim.dev.linux@gmai.com>
2026-02-06 11:34:28 +0530

    chore: Install project dependencies by adding numerous files to `node_modules`.

--- ai-career-coach-master (2)/ai-career-coach-master/backend/routes/roles.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRoles, getRole, getCategories } = require('../controllers/roleController');
-
-router.get('/', getRoles);
-router.get('/categories', getCategories);
-router.get('/:id', getRole);
-
-module.exports = router;
