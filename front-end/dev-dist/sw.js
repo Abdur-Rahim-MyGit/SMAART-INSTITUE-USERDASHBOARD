551f7fdcad294293f467a9cc2b03df32994f1e5e
dharsinismaart <dharsinismaart@gmail.com>
2026-06-19 12:43:40 +0530

    chore: save progress and updates

--- front-end/dev-dist/sw.js
+++ front-end/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3pjs6sg3veo"
+    "revision": "0.hepq6kdudb4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
